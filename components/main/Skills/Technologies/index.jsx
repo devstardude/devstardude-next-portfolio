de290922527c014053ea967704a992fe21ca644d
Arun Shekhar <arunshekhar001@gmail.com>
2022-10-08 19:27:59 +0530

    skills added

--- components/main/Skills/Technologies/index.jsx
+++ components/main/Skills/Technologies/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TechBox from "./TechBox";
-import { SiReact, SiDiscord } from "react-icons/si";
+import { SiReact, SiHiveBlockchain } from "react-icons/si";
 import { GoDatabase, GoDeviceDesktop } from "react-icons/go";
 import {
   AiFillDatabase,
@@ -38,7 +38,7 @@ const Technologies = (props) => {
         transition={{ ease: "easeOut", duration: 1 }}
         viewport={{ once: true }}
       >
-        <div >
+        <div>
           <motion.li
             initial="hidden"
             whileInView="visible"
@@ -63,48 +63,48 @@ const Technologies = (props) => {
 const techs = [
   {
     title: "Front-End",
-    desc: "React and Redux",
-    icon: <SiReact  fontSize={35} />,
+    desc: "React, Redux, Next.js, TypeScript, Jest Testing",
+    icon: <SiReact fontSize={35} />,
   },
   {
     title: "Back-End",
-    desc: "Node, Express, REST, GraphQL",
-    icon: <AiFillDatabase  fontSize={35} />,
+    desc: "Node, Express, REST, GraphQL, Go API",
+    icon: <AiFillDatabase fontSize={35} />,
   },
   {
     title: "Database",
     desc: "MongoDB, Firestore, DynamoDB ",
-    icon: <GoDatabase  fontSize={35} />,
+    icon: <GoDatabase fontSize={35} />,
   },
   {
     title: "UI/UX",
     desc: "Tailwind CSS, Material UI, Framer motion",
-    icon: <GoDeviceDesktop  fontSize={35} />,
+    icon: <GoDeviceDesktop fontSize={35} />,
   },
   {
     title: "Cloud Services",
     desc: "Firebase, AWS",
-    icon: <AiFillCloud  fontSize={35} />,
+    icon: <AiFillCloud fontSize={35} />,
   },
   {
     title: "Version Tracking",
-    desc: "Git, Github",
-    icon: <FaGitAlt  fontSize={35} />,
+    desc: "Git, GitHub, GitLab",
+    icon: <FaGitAlt fontSize={35} />,
   },
   {
     title: "Languages",
-    desc: "Python, JavaScript, C++, Go",
-    icon: <AiOutlineCode  fontSize={35} />,
+    desc: "Python, JavaScript, C++, Go, TypeScript, Solidity",
+    icon: <AiOutlineCode fontSize={35} />,
   },
   {
-    title: "Discord Bot",
-    desc: "Fully Functional Bot in Discord.js",
-    icon: <SiDiscord  fontSize={35} />,
+    title: "Misc",
+    desc: "Blockchain, NFTs applications, Fully Functional Bot in Discord.js",
+    icon: <SiHiveBlockchain fontSize={35} />,
   },
   {
     title: "Project Management",
     desc: "Notion, Asana, Trello, Slack",
-    icon: <AiOutlineProject  fontSize={35} />,
+    icon: <AiOutlineProject fontSize={35} />,
   },
 ];
 
